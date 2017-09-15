5a08a57a2b25716ef97c659e512b8b1834aa0135
Guansong Zhang <zhang.guansong@gmail.com>
2017-09-15 15:48:55 +0000

    Update atmi build script to include device runtime build

--- bin/build_atmi.sh
+++ bin/build_atmi.sh
@@ -36,6 +36,9 @@ function getdname(){
    echo $__DIRN
 }
 
+GFXLIST=${GFXLIST:-"gfx700 gfx701 gfx800 gfx801 gfx803 gfx900 gfx901"}
+export GFXLIST
+
 thisdir=$(getdname $0)
 [ ! -L "$0" ] || thisdir=$(getdname `readlink "$0"`)
 if [ -f $thisdir/HCC2_VERSION_STRING ] ; then 
@@ -103,7 +106,7 @@ if [ "$1" != "nocmake" ] && [ "$1" != "install" ] ; then
    BUILDTYPE="Release"
    echo rm -rf $BUILD_ATMI/build_atmi/build_lib
    rm -rf $BUILD_ATMI/build_atmi/build_lib
-   MYCMAKEOPTS="-DLLVM_DIR=$HCC2 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DATMI_HSA_SERVICE=on -DATMI_HSA_INTEROP=on -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR "
+   MYCMAKEOPTS="-DLLVM_DIR=$HCC2 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DGENERIC_IS_ZERO=ON -DCUDA_TRIPLE=ON -DATMI_HSA_SERVICE=on -DATMI_HSA_INTEROP=on -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR "
    mkdir -p $BUILD_ATMI/build_atmi/build_lib
    cd $BUILD_ATMI/build_atmi/build_lib
    echo " -----Running atmi cmake ---- " 
@@ -118,7 +121,7 @@ if [ "$1" != "nocmake" ] && [ "$1" != "install" ] ; then
    BUILDTYPE="Debug"
    echo rm -rf $BUILD_ATMI/build_atmi/build_debug
    rm -rf $BUILD_ATMI/build_atmi/build_debug
-   MYCMAKEOPTS="-DLLVM_DIR=$HCC2 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DATMI_HSA_SERVICE=on -DATMI_HSA_INTEROP=on -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR "
+   MYCMAKEOPTS="-DLLVM_DIR=$HCC2 -DCMAKE_BUILD_TYPE=$BUILDTYPE -DGENERIC_IS_ZERO=ON -DCUDA_TRIPLE=ON -DATMI_HSA_SERVICE=on -DATMI_HSA_INTEROP=on -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR "
    mkdir -p $BUILD_ATMI/build_atmi/build_debug
    cd $BUILD_ATMI/build_atmi/build_debug
    echo " -----Running atmi cmake for debug ---- " 
