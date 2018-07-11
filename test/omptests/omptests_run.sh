38a5f5fe820ecba673253b81613b0aaebbbb686b
Andrey Kasaurov <Andrey.Kasaurov@amd.com>
2018-07-11 16:59:07 +0300

    test/omptests: fix a typo and change log name

--- test/omptests/omptests_run.sh
+++ test/omptests/omptests_run.sh
@@ -4,4 +4,4 @@ DEVICE_TARGET=${DEVICE_TARGET:-nvptx64-nvidia-cuda}
 DEVICE_ARCH=${DEVICE_ARCH:-sm_35}
 #DEVICE_ARCH=${DEVICE_ARCH:-gfx803}
 
-env TARGET="-fopenmp-targets=$DEVUCE_TARGET -Xopenmp-target=$DEVICE_TARGET -march=$DEVICE_ARCH" HOSTRTL=$ATMI/lib/libdevice TARGETRTL=$OMPRT/lib GLOMPRTL=$OMPRT/lib LLVMBIN=$HCC2/bin make 2>&1 | tee omptests_run_`date --rfc-3339=date`.log
+env TARGET="-fopenmp-targets=$DEVICE_TARGET -Xopenmp-target=$DEVICE_TARGET -march=$DEVICE_ARCH" HOSTRTL=$ATMI/lib/libdevice TARGETRTL=$OMPRT/lib GLOMPRTL=$OMPRT/lib LLVMBIN=$HCC2/bin make 2>&1 | tee omptests_run_`date --iso-8601=minutes`.log
