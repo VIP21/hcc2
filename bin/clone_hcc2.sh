73a9ee0b27b14aa7176d46b9d0fb85f57ab54e9a
gregrodgers <Gregory.Rodgers@amd.com>
2018-07-13 11:52:16 -0500

    Move development of openmp for amdgcn to branch HCC2.180713. Notice that I only moved trunk repositories clang,llvm,lld,and openmp.  hip, atmi-staging, and rocm-device-libs will stay on branch 180619

--- bin/clone_hcc2.sh
+++ bin/clone_hcc2.sh
@@ -77,19 +77,19 @@ COBRANCH="master"
 #clone_or_pull
 
 reponame="openmp"
-COBRANCH="HCC2-180619"
+COBRANCH="HCC2-180713"
 clone_or_pull
 
 reponame="llvm"
-COBRANCH="HCC2-180619"
+COBRANCH="HCC2-180713"
 clone_or_pull
 
 reponame="clang"
-COBRANCH="HCC2-180619"
+COBRANCH="HCC2-180713"
 clone_or_pull
 
 reponame="lld"
-COBRANCH="HCC2-180619"
+COBRANCH="HCC2-180713"
 clone_or_pull
 
 reponame="hip"
