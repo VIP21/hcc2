13b4f4c59a42c2b953d29fd0f215d6797b7f31e6
gregrodgers <Gregory.Rodgers@amd.com>
2018-08-29 13:58:56 -0500

    fix the vector_copy cuda kernel to run for all indicies

--- examples/cloc/vector_copy_cuda/vector_copy.cu
+++ examples/cloc/vector_copy_cuda/vector_copy.cu
@@ -1,6 +1,4 @@
-#include <cuda_runtime.h>
-
-extern "C" __global__ void vector_copy(__device__ int *in, __device__ int *out) {
-  int id = blockIdx.x; 
+extern "C" __global__ void vector_copy(int *in, int *out) {
+  int id = (blockIdx.x*blockDim.x) + threadIdx.x;
   out[id] = in[id];
 }
